e708401ac61c32e7cdfb5fb5b59fe815912b1802
lincolnkermit <lincolnkermit@protonmail.com>
2025-03-28 16:49:43 +0100

    Playing with async functions - Added comments

--- main.dart
+++ main.dart
@@ -1,25 +1,31 @@
 import 'dart:core';
-
+import 'dart:async';
 
 Future<String> CountToMillions() async {
-    print("Stopwatch started");
     Stopwatch stopwatch = new Stopwatch()..start();
     for (var i = 0; i < 1000; i++) {
         await Future.delayed(Duration(milliseconds: 1));
     }
     print('Time elapsed: ${stopwatch.elapsed}');
-    print("Stopwatch stopped");
+    print(DateTime.now);
     return "Success";
 }
 
-
+ 
 
 
 
 void main() {
     print("Starting");
-    CountToMillions().then((result) {
+    CountToMillions().then((result) { // saying ok when u are done print the result while I do others things
     print(result);
     });
-    print("The end");
+    print("Starting parallel tasks - counting the alphabet"); // starting doing other things
+    for (var y = 0; y < 30000; y++) {
+        print(y);
+        for (var i = 0; i < 26; i++) {
+            print(String.fromCharCode(65 + i)); // double loop
+        }
+    }
+
 }

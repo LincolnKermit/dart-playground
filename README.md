b61d862d83f684d3877b37096fdca5c728012967
lincolnkermit <lincolnkermit@protonmail.com>
2025-03-31 15:43:59 +0200

    Added loading bar in async way

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dart-playground
 Dart Playground to learn
+
+
+
+This repository is only a personal bookmark to remember useful functions or concept in Dart.
+Feel free to see, learn or use.
--- main.dart
+++ main.dart
@@ -1,31 +1,81 @@
 import 'dart:core';
 import 'dart:async';
 
-Future<String> CountToMillions() async {
+
+int i = 0;
+
+
+Future<String> task1() async {
     Stopwatch stopwatch = new Stopwatch()..start();
-    for (var i = 0; i < 1000; i++) {
+    for (var i = 0; i < 10190; i++) {
         await Future.delayed(Duration(milliseconds: 1));
     }
-    print('Time elapsed: ${stopwatch.elapsed}');
-    print(DateTime.now);
     return "Success";
 }
 
- 
+Future<String> task2() async {
+    Stopwatch stopwatch = new Stopwatch()..start();
+    for (var i = 0; i < 19940; i++) {
+        await Future.delayed(Duration(milliseconds: 1));
+    }
+    return "Success";
+}
 
+Future<String> task3() async {
+    Stopwatch stopwatch = new Stopwatch()..start();
+    for (var i = 0; i < 11900; i++) {
+        await Future.delayed(Duration(milliseconds: 1));
+    }
+    return "Success";
+}
+
+Future<String> task4() async {
+    Stopwatch stopwatch = new Stopwatch()..start();
+    for (var i = 0; i < 20010; i++) {
+        await Future.delayed(Duration(milliseconds: 1));
+    }
+    return "Success";
+}
+
+
+
+void loading(int i) {
+    print((i * 25).toString() + " %");
+}
+
+
+void increment() {
+    i++;
+}
 
 
 void main() {
-    print("Starting");
-    CountToMillions().then((result) { // saying ok when u are done print the result while I do others things
-    print(result);
+    i = 0;
+    print("Starting task 1");
+    task1().then((result) { // saying ok when u are done print the result while I do others things
+    print("Task 1 completed");
+    increment();
+    loading(i);   
     });
-    print("Starting parallel tasks - counting the alphabet"); // starting doing other things
-    for (var y = 0; y < 30000; y++) {
-        print(y);
-        for (var i = 0; i < 26; i++) {
-            print(String.fromCharCode(65 + i)); // double loop
-        }
-    }
+    print("Starting task 2");
+    task2().then((result){
+        print("Task 2 completed");
+        increment();
+        loading(i);
+    });
+    print("Starting task 3");
+    task3().then((result){
+        print("Task 3 completed");
+        increment();
+        loading(i);
+    });
+    print("Starting task 4");
+    task4().then((result){
+        print("Task 4 completed");
+        increment();
+        loading(i);
+    });
+    print("All tasks started");
+    print("Waiting for all tasks to complete");
 
 }
